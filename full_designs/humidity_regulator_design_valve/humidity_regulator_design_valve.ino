8f926cc724a7f28dcc8dec4d471e4cb939b6f2b6
Tijs van Roon <knifter@gmail.com>
2020-07-07 10:43:09 +0200

    TvR Changes to make ours work

--- full_designs/humidity_regulator_design_valve/humidity_regulator_design_valve.ino
+++ full_designs/humidity_regulator_design_valve/humidity_regulator_design_valve.ino
@@ -1,13 +1,18 @@
+#include <Arduino.h>
+#include <Wire.h>
+
 #include <PID_v1.h>
 #include <LiquidCrystal.h>
 
-#define PIN_TIP120 7
-#define ANALOG_SENSOR_RH 0
-#define ANALOG_HUM_ADJUST 1
+#define PIN_VALVE           11
+#define ANALOG_SENSOR_RH    1
+#define ANALOG_HUM_ADJUST   2
+#define PID_LOOPTIME_MS     2000            // PID delta-T: 2 seconds as it was
+#define DISPLAY_LOOPTIME_MS 500             // Display update rate: every half a second
+#define WINDOWSIZE          5000			// time-windows size in ms
 
 // Define Variables for the PID controler
 double Setpoint, Input, Output;
-int WindowSize = 5000;
 unsigned long windowStartTime;
 
 // Define PID constants
@@ -17,76 +22,89 @@ double Kd = 1;
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
 // Initialize liquid crystal
-//                BS  E  D4 D5  D6 D7
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-
+LiquidCrystal lcd(/*RS*/ 8, /*enb*/ 9, /*D4*/ 4, /*D5*/ 5, /*D6*/ 6, /*D7*/ 7);
 
 void setup()
 {
-  lcd.begin(16, 2);
+    lcd.begin(16, 2);
 
-  windowStartTime = millis();
+    digitalWrite(PIN_VALVE, HIGH);
+    pinMode(PIN_VALVE, OUTPUT);
 
-  // Set the range between 0 and the full window size
-  myPID.SetOutputLimits(0, WindowSize);
+    windowStartTime = millis();
+    Output = WINDOWSIZE / 2;
 
-  // Turn the PID on
-  myPID.SetMode(AUTOMATIC);
+    // Set the range between 0 and the full window size
+    myPID.SetOutputLimits(0, WINDOWSIZE);
+
+    // Turn the PID on
+    myPID.SetMode(AUTOMATIC);
 }
 
 void loop()
 {
-  // Read the Setpoint on ANALOG_HUM_ADJUST:
-  // Convert the analog reading (which goes from 0 - 1023) to voltage (between 0 and 5V)
-  float measVoltageSetPoint = analogRead(ANALOG_HUM_ADJUST) / 1023. * 5;
-  // Apply the manufacturer's calibration
-  // This has to be changed when the sensor is replaced
-  float measRHSetPoint = (measVoltageSetPoint - 0.826) / 0.0315;
+    // Read the Setpoint on ANALOG_HUM_ADJUST:
+    // Convert the analog reading (which goes from 0 - 1023) to voltage (between 0 and 5V)
+    float measVoltageSetPoint = analogRead(ANALOG_HUM_ADJUST) / 1023. * 5;
+    // Apply the manufacturer's calibration
+    // This has to be changed when the sensor is replaced
+    float measRHSetPoint = (measVoltageSetPoint - 0.826) / 0.0315;
 
-  // Setpoint for the PID
-  Setpoint = measRHSetPoint;
+    // Read the humidity sensor value
+    float measVoltageSensor = analogRead(ANALOG_SENSOR_RH) / 1023. * 5;
+    float measRHSensor = (measVoltageSensor - 0.826) / 0.0315;
 
+    unsigned long now = millis();
 
-  // Read the humidity sensor value
-  float measVoltageSensor = analogRead(ANALOG_SENSOR_RH) / 1023. * 5;
-  float measRHSensor = (measVoltageSensor - 0.826) / 0.0315;
+    /************************************************
+     * PID controler
+     ************************************************/
+    static unsigned long lastPID = 0;
+    if(now - lastPID > PID_LOOPTIME_MS)
+    {
+        // Setpoint for the PID
+        Setpoint = measRHSetPoint;
 
-  // Input for the PID
-  Input = measRHSensor;
-  myPID.Compute();
+        // Input for the PID
+        Input = measRHSensor;
+        myPID.Compute();
 
-  /************************************************
-   * PID controler
-   ************************************************/
-  unsigned long now = millis();
-  if(now - windowStartTime > WindowSize)
-  { //time to shift the Relay Window
-    windowStartTime += WindowSize;
-  }
-  if(Output > now - windowStartTime)
-  {
-    // Increase
-    analogWrite(PIN_TIP120, 255);
-  }
-  else
-  {
-    // Decrease
-    analogWrite(PIN_TIP120, 0);
-  }
+        lastPID = now;
+    };
 
-  /************************************************
-   * DISPLAY
-   ************************************************/
-  float MeasRH  = analogRead(ANALOG_SENSOR) / 1023. * 100;
+    if(now - windowStartTime > WINDOWSIZE)
+    { //time to shift the Relay Window
+        windowStartTime += WINDOWSIZE;
+    };
+    if(Output > (now - windowStartTime))
+    {
+        // Increase
+        digitalWrite(PIN_VALVE, HIGH);
+    }else{
+        // Decrease
+        digitalWrite(PIN_VALVE, LOW);
+    };
 
-  lcd.setCursor(0, 0);
-  lcd.print("RH=    % (Meas.)");
-  lcd.setCursor(3, 0);
-  lcd.print(MeasRH, 1);
-  lcd.setCursor(0, 1);
-  lcd.print("RH=    % (SetPt)");
-  lcd.setCursor(3, 1);
-  lcd.print(RH, 1);
+    /************************************************
+     * DISPLAY
+     ************************************************/
+    static unsigned long lastDisplay = 0;
+    if(now - lastDisplay > DISPLAY_LOOPTIME_MS)
+    {
+        float MeasRH  = analogRead(ANALOG_SENSOR_RH) / 1023. * 100;
 
-  delay(2000);
-}
+        lcd.setCursor(0, 0);
+        lcd.print("RH=    % (Meas.)");
+        lcd.setCursor(3, 0);
+        lcd.print(MeasRH, 1);
+        lcd.setCursor(0, 1);
+        lcd.print("RH=    % (SetPt)");
+        lcd.setCursor(3, 1);
+        lcd.print(measRHSetPoint, 1);
+        // lcd.print(Output, 1);
+
+        lastDisplay = now;
+    };
+
+    delay(10);
+};
