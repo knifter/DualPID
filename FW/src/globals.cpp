787b3c9d8479166491a631db1bd2a10536ebe2d0
Tijs van Roon <knifter@gmail.com>
2021-12-06 13:30:55 +0100

    SHT3X upstream

--- FW/src/globals.cpp
+++ FW/src/globals.cpp
@@ -10,7 +10,7 @@ GUI gui;
 SettingsManager setman;
 settings_t& settings = setman.settings;
 
-double get_value1() { return sht_sensor.getTemperature(); };
+double get_value1() { return sht_sensor.newMeasurement()->temperature; };
 double get_value2() { return sht_sensor.getHumidity(); };
 
 PIDLoop pid1(PIN_HB1_A, PIN_HB1_B, &get_value1);
