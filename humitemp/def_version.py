d747f29dc1f6de87d0aabcf9bef10c6f66537b4d
Tijs van Roon <1374848+knifter@users.noreply.github.com>
2022-05-19 15:54:08 +0200

    def-version from autosampler

--- humitemp/def_version.py
+++ humitemp/def_version.py
@@ -2,14 +2,20 @@ import subprocess
 import datetime
 
 #(part of) GIT revision
-# version = (
-#     subprocess.check_output(["git", "rev-parse", "HEAD"])
-#     .strip()
-#     .decode("utf-8")
-# )
-# print("-DVERSION='\"%s\"'" % version[-4:])
+revision = (
+    subprocess.check_output(["git", "rev-parse", "HEAD"])
+    .strip()
+    .decode("utf-8")
+)
+
+branch = (
+    subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"])
+    .strip()
+    .decode("utf-8")
+)
 
 #date
 curr_date = datetime.datetime.now()
-version = "%02d%02d%02d" % (curr_date.year, curr_date.month, curr_date.day)
-print("-DVERSION=%s" % version)
+datetime = "%02d%02d%02d" % (curr_date.year, curr_date.month, curr_date.day)
+
+print("-DBUILD_DATETIME=%s -DGIT_BRANCH=%s -DGIT_REVISION=%s" % (datetime, branch, revision[-8:]))
