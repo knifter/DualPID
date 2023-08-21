b9e5edd628d8464e6e6406ffc33c694b63f201e1
Tijs van Roon <1374848+knifter@users.noreply.github.com>
2023-08-21 13:51:10 +0200

    Versionstr, hold boot in expert-mode

--- dualpid/src/screens/boot.cpp
+++ dualpid/src/screens/boot.cpp
@@ -18,7 +18,7 @@ BootScreen::BootScreen(SooghGUI& g) : Screen(g)
     lv_obj_set_size(label, LV_PCT(100), 50);
 	lv_obj_center(label);
     lv_obj_set_style_text_align(label, LV_TEXT_ALIGN_CENTER, 0);
-    lv_label_set_text_fmt(label, "HumReg v%d", VERSION);
+    lv_label_set_text_fmt(label, "DualPID " VERSIONSTR_LONG);
     if(expert_mode)
         lv_obj_set_style_text_color(label, COLOR_DEEP_ORANGE, 0);
 };
@@ -32,6 +32,8 @@ void BootScreen::load()
 void BootScreen::loop()
 {
     uint32_t now = millis();
+    if(expert_mode)
+        return;
     if((now - _start) > BOOTSCREEN_TIMEOUT_MS)
     {
         gui.pushScreenType(ScreenType::MAIN);
@@ -46,8 +48,7 @@ bool BootScreen::handle(soogh_event_t e)
 		case KEY_A_SHORT:
 		case KEY_B_SHORT:
 		case KEY_C_SHORT:
-            // Push mainscreen next loop
-            _start = 0;
+            gui.pushScreenType(ScreenType::MAIN);
 			return true;
 		default:
 			return false;
