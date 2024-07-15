29418c823cdb8b5afbf92999554ea98c75f3bde8
Tijs van Roon <1374848+knifter@users.noreply.github.com>
2024-07-15 15:46:34 +0200

    onOpen proper cb

--- dualpid/src/screens/menu.cpp
+++ dualpid/src/screens/menu.cpp
@@ -224,15 +224,13 @@ MenuScreen::MenuScreen(SooghGUI& g) : Screen(g)
     // General
     if(rtc_available())
     {
-            // FIXME: add onOpen() to TreeMenu
-            datetime_open_cb(nullptr, nullptr);
-
             auto sub = menu.addSubMenu("Set Date/Time");
             sub->addSpinbox("Day", &tmp_mday, 1, 31, 0);
             sub->addSelector("Month", &tmp_mon, months);
             sub->addSpinbox("Year", &tmp_year, 1900, 2099, 0);
             sub->addSpinbox("Hour", &tmp_hour, 0, 23, 0);
             sub->addSpinbox("Minute", &tmp_min, 0, 59, 0);
+            sub->onOpen(datetime_open_cb);
             sub->onClose(datetime_close_cb);
     };
     
