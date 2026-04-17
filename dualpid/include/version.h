128e0abfc4b429e967eea82d712cfd94c7f40b85
Tijs van Roon <1374848+knifter@users.noreply.github.com>
2026-04-17 10:32:51 +0200

    Git dirty, git branch main detection, version major/minor from version.h

--- dualpid/include/version.h
+++ dualpid/include/version.h
@@ -1,15 +1,29 @@
 #ifndef __VERSION_H
 #define __VERSION_H
 
-#include "version_auto.h"
+#include "_version.h"
 // A few defines here are set during build in def_version.py
 
-#define _STRINGIFY(x)      #x
-#define STRINGIFY(x)      _STRINGIFY(x)
+#define VERSION_MAJOR           1
+#define VERSION_MINOR           9
 
-#define VERSIONSTR_LONG         STRINGIFY(GIT_BRANCH-VERSION)
-#define VERSIONSTR_SHORT        STRINGIFY(VERSION)
-#define VERSION                 BUILD_DATE_YMD
-#define VERSION_STR             STRINGIFY(GIT_BRANCH) " (" STRINGIFY(BUILD_DATE_YM) ")"
+#define _STRINGIFY(x)           #x
+#define STRINGIFY(x)            _STRINGIFY(x)
+#define FLOATIFY(integ, fract)  (integ##.##fract##f)
+#define VERSION_MAJOR_STR       STRINGIFY(VERSION_MAJOR)
+#define VERSION_MINOR_STR       STRINGIFY(VERSION_MINOR)
+#define VERSION_NUMBER          ((VERSION_MAJOR << 16) | VERSION_MINOR)
+#define VERSION_FLOAT           FLOATIFY(VERSION_MAJOR, VERSION_MINOR)
+
+#if GIT_BRANCH_MAIN
+    #define VERSION_PATCH         BUILD_DATE_YM_STR
+    #if GIT_DIRTY
+        #pragma message "Compiling main branch release from dirty git checkout!"
+    #endif
+    #define VERSION_STR           VERSION_MAJOR_STR "." VERSION_MINOR_STR "." VERSION_DIRTY_STR
+#else
+    #define VERSION_PATCH         GIT_REVISION
+    #define VERSION_STR           GIT_BRANCH "-" VERSION_PATCH VERSION_DIRTY_STR
+#endif
 
 #endif // __VERSION_H
--- dualpid/tools/gen_version_auto_h.py
+++ dualpid/tools/gen_version_auto_h.py
@@ -1,13 +1,24 @@
 import datetime
 import os
+import re
 import subprocess
 from sys import stderr
 
-output_path = "include/version_auto.h"
+# Version 17-4-2026
+
+INPUT_PATH = "include/version.h"
+OUTPUT_PATH = "include/_version.h"
+DIRTY_STRING = "-dirty"
+
+# Read VERSION_MAJOR / VERSION_MINOR from version.h
+with open(INPUT_PATH) as f:
+    src = f.read()
+VERSION_MAJOR = int(re.search(r'#define\s+VERSION_MAJOR\s+(\d+)', src).group(1))
+VERSION_MINOR = int(re.search(r'#define\s+VERSION_MINOR\s+(\d+)', src).group(1))
 
 curr_date = datetime.datetime.now()
-build_date_ym = "%02d%02d" % (curr_date.year, curr_date.month)
-build_date_ymd = "%02d%02d%02d" % (curr_date.year, curr_date.month, curr_date.day)
+build_date_ym  = "%02d%02d"     % (curr_date.year % 100, curr_date.month)
+build_date_ymd = "%02d%02d%02d" % (curr_date.year % 100, curr_date.month, curr_date.day)
 
 try:
     git_revision = (
@@ -29,20 +40,37 @@ except subprocess.CalledProcessError:
     print("Warning: Could not get git branch", file=stderr)
     git_branch = "unknown"
 
+try:
+    dirty = bool(subprocess.check_output(["git", "status", "--porcelain"]).strip())
+except subprocess.CalledProcessError:
+    dirty = False
+
+dirty_suffix  = DIRTY_STRING if dirty else ""
+branch_main   = git_branch in ("main", "master")
+
+if branch_main:
+    version_str = "%d.%d.%s%s" % (VERSION_MAJOR, VERSION_MINOR, build_date_ym, dirty_suffix)
+else:
+    version_str = "%s-%s%s" % (git_branch, build_date_ymd, dirty_suffix)
+
 content = (
     "#ifndef __VERSION_AUTO_H\n"
     "#define __VERSION_AUTO_H\n\n"
-    "#define BUILD_DATE_YM %s\n"
-    "#define BUILD_DATE_YM_STR \"%s\"\n"
-    "#define BUILD_DATE_YMD %s\n"
+    "#define BUILD_DATE_YM      %s\n"
+    "#define BUILD_DATE_YM_STR  \"%s\"\n"
+    "#define BUILD_DATE_YMD     %s\n"
     "#define BUILD_DATE_YMD_STR \"%s\"\n"
-    "#define GIT_REVISION \"%s\"\n"
-    "#define GIT_BRANCH \"%s\"\n"
+    "#define GIT_REVISION       \"%s\"\n"
+    "#define GIT_BRANCH         \"%s\"\n"
+    "#define GIT_DIRTY          %d\n"
+    "#define GIT_BRANCH_MAIN    %d\n"
+    "#define VERSION_DIRTY_STR  \"%s\"\n"
+    "#define VERSION_STR_AUTO   \"%s\"\n"
     "\n#endif // __VERSION_AUTO_H\n"
-) % (build_date_ym, build_date_ym, build_date_ymd, build_date_ymd, 
-     git_revision, git_branch)
+) % (build_date_ym, build_date_ym, build_date_ymd, build_date_ymd,
+     git_revision, git_branch, dirty, branch_main, dirty_suffix, version_str)
 
 # Only write the file if the contents actually changed:
-if not os.path.exists(output_path) or open(output_path).read() != content:
-    with open(output_path, "w") as f:
+if not os.path.exists(OUTPUT_PATH) or open(OUTPUT_PATH).read() != content:
+    with open(OUTPUT_PATH, "w") as f:
         f.write(content)
