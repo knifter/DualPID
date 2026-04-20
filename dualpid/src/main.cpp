308ae9b824ce86b0ee7b2bb7bd53a8e47245b73b
Tijs van Roon <1374848+knifter@users.noreply.github.com>
2026-04-20 13:59:39 +0200

    timing code

--- dualpid/src/main.cpp
+++ dualpid/src/main.cpp
@@ -52,20 +52,67 @@ void setup()
 
 void loop()
 {
+	time_t t1 = micros();
     soogh_event_t e = static_cast<soogh_event_t>(key2event(scan_keys()));
     // if(e & KEYTOOL_EVENT_MASK)
     //     DBG("event: %s", soogh_event_name(e));
 
+	time_t t2 = micros();
 	gui.handle(e);
+	time_t t3 = micros();
 	gui.loop();
+	time_t t4 = micros();
 
 	setman.loop();
+	time_t t5 = micros();
 	for(PIDLoop* pid : pids)
 	{
 		pid->loop();
 	};
+	time_t t6 = micros();
 
-	cli_loop();
+	// cli_loop(); 
+	time_t t7 = micros();
+
+	static time_t next = micros() + 3000000;
+	static uint32_t time1 = 0;
+	static uint32_t time2 = 0;
+	static uint32_t time3 = 0;
+	static uint32_t time4 = 0;
+	static uint32_t time5 = 0;
+	static uint32_t time6 = 0;
+	static int n = 0;
+	
+	time1 += t2 - t1;
+	time2 += t3 - t2;
+	time3 += t4 - t3;
+	time4 += t5 - t4;
+	time5 += t6 - t5;
+	time6 += t7 - t6;
+	n++;
+
+	if(t7 > next)
+	{
+		time1 /= n;
+		time2 /= n;
+		time3 /= n;
+		time4 /= n;
+		time5 /= n;
+		time6 /= n;
+
+		DBG("time1_event:    %d", time1);
+		DBG("time2_guihandle:%d", time2);
+		DBG("time3_guiloop:  %d", time3);
+		DBG("time4_setman:   %d", time4);
+		DBG("time5_pidloops: %d", time5);
+		DBG("time6_cli:      %d", time6);
+		uint32_t total = time1+time2+time3+time4+time5+time6;
+		DBG("%d us total. %d loops per sec. %d us per loop: %d extra", total, n / 3, 3000000/n, 3000000/n - total);
+
+		time1 = 0; time2 = 0; time3 = 0; time4=0;time5=0;time6=0;
+		n = 0;
+		next += 3000000;
+	};
 };
 
 void halt(const char* error)
